fc2411fa5c48ba975b269f8b76c72c034310e6d6
felix <felix.gyamfi.desin@gmail.com>
2021-10-03 22:57:36 +0200

    rebuild ArrowLinks 2nd

---
+++ src/components/Navigation/CustomLink.js
@@ -0,0 +1,23 @@
+import React from "react"
+import styled from "styled-components"
+import scrollTo from "gatsby-plugin-smoothscroll"
+
+const CstmLink = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+`
+export const scrollLink = (pageHash = "") => {
+  return () => scrollTo(pageHash)
+}
+
+export const CustomLink = ({ pageHash, pageTitle }) => {
+  return (
+    <ul>
+      <li>
+        <CstmLink onClick={scrollLink(pageHash)}>{pageTitle}</CstmLink>
+      </li>
+    </ul>
+  )
+}
