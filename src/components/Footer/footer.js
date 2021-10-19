f9206bceca7f03edeb744dea5489435edfba9a35
felix <felix.gyamfi.desin@gmail.com>
2021-10-19 19:07:00 +0200

    styling footer to fit overall style

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,11 +5,13 @@ const Wrapper = styled.footer`
   position: fixed;
   bottom: 0;
   width: 100%;
-  background-color: rgba(223, 221, 217, 1);
   z-index: 900;
   grid-area: 2/1;
   padding: 0 3.3rem 0.5rem;
   font-size: 1rem;
+  @media screen and (min-width: 992px) {
+    padding: 0 6rem 0.5rem;
+  }
 `
 
 function Footer() {
